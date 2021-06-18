085bfd9ef2565bd43c70b9a7528d26bb33e4121c
gotham-prabhu <gautam.g4@gmail.com>
2021-06-18 23:15:05 +0530

    Added sample ui5-project

---
+++ webapp/Component-preload.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/base/ManagedObject"
+], function(
+	ManagedObject
+) {
+	"use strict";
+
+	return ManagedObject.extend("sap.ui.excel.ui5-sheetjs-t.Component-preload", {
+	});
+});
